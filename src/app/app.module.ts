c4865645357f90dde56bc6c9cb54077b9e2297f0
harinisatya <harinisatya11@gmail.com>
2022-06-28 16:39:54 +0530

    2 files modified, 3 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { StudentComponent } from './student/student.component';
 import { SampleComponent } from './sample/sample.component';
+import { TestsampleComponent } from './testsample/testsample/testsample.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, StudentComponent, SampleComponent],
+  declarations: [AppComponent, SampleComponent, TestsampleComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/testsample/testsample/testsample.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testsample',
+  templateUrl: './testsample.component.html',
+  styleUrls: ['./testsample.component.css']
+})
+export class TestsampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
